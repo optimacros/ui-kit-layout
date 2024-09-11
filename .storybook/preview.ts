e767224ef98886560f6e9e0951cae6e647ab0195
vugolnikov <73517684+Pail4@users.noreply.github.com>
2024-09-11 12:17:42 +0500

    init footer story

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,16 @@ import { Preview } from '@storybook/react'
 
 import '../src/themes/appBaseTheme.css';
 
+import '../src/styles/colors.css'
+import '../src/styles/variables.css'
+import '../src/styles/media.css'
+import '../src/styles/config.css'
+
+import '../src/styles/Normalize.css'
+import '../src/styles/DefaultColorTheme.css'
+import '../src/styles/Application.css'
+import '../src/styles/Fonts.css'
+
 export const parameters: Preview = {
     parameters: {
         actions: { argTypesRegex: '^on[A-Z].*' },
---
+++ src/components/Footer/Footer.stories.ts
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Footer } from './Footer'
+
+const meta: Meta<typeof Footer> = {
+    component: Footer,
+    argTypes: {
+        appVersion: {
+            description: 'Specify App version',
+        },
+    },
+}
+
+export default meta
+
+type Story = StoryObj<typeof Footer>
+
+export const Basic: Story = {
+    args: {
+        appVersion: '1.0.0',
+    },
+}
