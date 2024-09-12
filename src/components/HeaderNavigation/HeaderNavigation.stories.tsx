8d5221a6d0aac5c029da279ea0ad6611e07d0dfe
vugolnikov <73517684+Pail4@users.noreply.github.com>
2024-09-12 11:00:39 +0500

    HeaderNavigation story

---
+++ src/components/HeaderNavigation/HeaderNavigation.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { ReactNode } from 'react'
+
+import { HeaderNavigation } from './HeaderNavigation'
+
+const meta: Meta<typeof HeaderNavigation> = {
+    component: HeaderNavigation,
+    argTypes: {
+        title: {
+            description: 'Header title',
+        },
+        onClick: {
+            description: 'Action on click',
+        },
+    },
+}
+
+export default meta
+
+type Story = StoryObj<typeof HeaderNavigation>
+
+const Wrapper = ({ children }: {children: ReactNode}) => {
+    return (
+        <div style={{ backgroundColor: 'var(--primary-color)' }}>
+            {children}
+        </div>
+    )
+}
+
+export const Basic:Story = {
+    args: {
+        // eslint-disable-next-line no-console
+        onClick: () => console.log('click'),
+        title: 'Title',
+    },
+    decorators: [
+        (story) => <Wrapper>{story()}</Wrapper>,
+    ],
+}
