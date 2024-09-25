3552fe45e8d6ea81aeee0f475aedc02b5c53e042
feex-s <feex.mail@gmail.com>
2024-09-25 17:34:54 +0300

    update Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,37 @@
+import classNames from 'classnames'
 import type { FC, ReactNode } from 'react'
 
 import styles from './Footer.module.css'
 
 export interface FooterProps {
-    appVersion: string;
+    className?: string;
+    appVersion?: string;
+    copyright?: string;
     children?: ReactNode;
 }
 
 export const Footer: FC<FooterProps> = (props) => {
-    return (
-        <footer className={styles.Footer}>
-            <span className={styles.Version}>
-                {props.appVersion}
-            </span>
+    const classNameContainer = classNames(styles.Container, props.className)
 
-            {props.children}
+    return (
+        <footer className={classNameContainer}>
+            {props.appVersion && (
+                <span className={styles.Version}>
+                    {props.appVersion}
+                </span>
+            )}
+
+            {props.copyright && (
+                <span className={styles.Copyright}>
+                    {props.copyright}
+                </span>
+            )}
+
+            {props.children && (
+                <div className={styles.Content}>
+                    {props.children}
+                </div>
+            )}
         </footer>
     )
 }
